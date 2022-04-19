d853aeffac1607ebac232e70dda4daee0e6f6b07
ChenXiao Liu <810196813@qq.com>
2022-04-19 00:18:00 -0500

    basic setup for frontend

---
+++ personalBlogSiteFrontend/src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import NavBar from './components/NavBar.vue'
+</script>
+
+<template>
+  <NavBar />
+</template>
+
+<style lang="less">
+* {
+  margin: 0;
+  padding: 0;
+}
+</style>
---
+++ personalBlogSiteFrontend/src/components/NavBar.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav></nav>
+</template>
+
+<style scoped lang="less">
+nav {
+  width: 100%;
+  height: 60px;
+  background-color: rgb(45, 45, 45);
+  box-shadow: 0 0 30px rgba(0, 0, 0, .4);
+}
+</style>
