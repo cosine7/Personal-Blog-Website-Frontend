4a9c6b4efc92432709732bfef5ddb2da471612a2
ChenXiao Liu <810196813@qq.com>
2022-04-26 22:00:10 -0500

    optimize code

--- personalBlogSiteFrontend/src/stores/blog.ts
+++ personalBlogSiteFrontend/src/stores/blog.ts
@@ -1,52 +1,48 @@
 import { defineStore } from 'pinia';
-import { reactive } from 'vue';
 import axios from 'axios';
-import type { Router } from 'vue-router';
 
-export default defineStore('blog', () => {
-  interface Blog {
-    _id: string,
-    category: string,
-    content: string,
-    createdAt: string,
-    updatedAt: String,
-    __v: Number
-  }
-  interface Blogs {
-    [key: string]: Blog[]
-  }
-  interface This {
-    $router: Router
-  }
-  const blogs = reactive(<Blogs>{});
+interface Blog {
+  _id: string,
+  category: string,
+  content: string,
+  createdAt: string,
+  updatedAt: String,
+  __v: Number
+}
 
-  async function addBlog(this: This, content: String, category: string) {
-    try {
-      const { data } = await axios.post('/blog', { content, category });
-      const { id, blog } = data;
-      if (!(id in blogs)) {
-        blogs[id] = <Blog[]>[];
+interface Blogs {
+  [key: string]: Blog[]
+}
+
+export default defineStore('blog', {
+  state: () => ({ blogs: <Blogs>{} }),
+
+  actions: {
+    async addBlog(content: String, category: string) {
+      try {
+        const { data } = await axios.post('/blog', { content, category });
+        const { id, blog } = data;
+        if (!(id in this.blogs)) {
+          this.blogs[id] = <Blog[]>[];
+        }
+        this.blogs[id].push(blog);
+        this.blogs.all.push(blog);
+        this.router.push('/');
+      } catch {
+        window.alert('Unable to Add Blog');
       }
-      blogs[id].push(blog);
-      blogs.all.push(blog);
-      this.$router.push('/');
-    } catch {
-      window.alert('Unable to Add Blog');
-    }
-  }
+    },
 
-  async function getBlogs(id: string) {
-    try {
-      const { data } = await axios.get(`/blog/${id}`);
-      if (!(id in blogs)) {
-        blogs[id] = <Blog[]>[];
+    async getBlogs(id: string) {
+      try {
+        const { data } = await axios.get(`/blog/${id}`);
+        if (!(id in this.blogs)) {
+          this.blogs[id] = <Blog[]>[];
+        }
+        this.blogs[id] = data;
+      } catch {
+        window.alert('Unable to Fetch Blogs');
       }
-      blogs[id] = data;
-    } catch {
-      window.alert('Unable to Fetch Blogs');
-    }
-  }
-  return {
-    blogs, addBlog, getBlogs,
-  };
+    },
+  },
 });
